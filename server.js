23a8044a9a7347044ee5687aa6950569ec5abe99
ShindeShubhamm <shubhamshinde.3151@gmail.com>
2020-04-18 22:25:48 +0530

    Prepared for deployment.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db')
 const contactsRoute = require('./routes/contacts')
 const authRoute = require('./routes/auth')
 const usersRoute = require('./routes/users')
+const path = require('path')
 
 // Initilize express server
 const app = express()
@@ -13,14 +14,18 @@ connectDB()
 // Init middleware to accept json as js object
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.send('This is your first page...')
-})
-
 // Define routes
 app.use('/api/users', usersRoute)
 app.use('/api/auth', authRoute)
 app.use('/api/contacts', contactsRoute)
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 // Listen on port
 const PORT = process.env.PORT
